f25ec73666b8cc1c0f874003da39ce0e1c69e340
Aurel Zefi <aurelzefi1994@gmail.com>
2021-02-08 01:25:30 +0100

    wip

--- app/Models/Alert.php
+++ app/Models/Alert.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,6 +13,15 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Alert extends Model
 {
     use HasFactory;
+
+    const TYPE_INSTANT = 'instant';
+
+    const TYPE_WEEKLY = 'weekly';
+
+    const TYPES = [
+        'instant',
+        'weekly',
+    ];
 
     protected $guarded = [];
 
@@ -34,13 +44,28 @@ class Alert extends Model
         return $this->hasMany(Keyword::class);
     }
 
-    public function setTypesAttribute(array $value): void
+    public function setJobTypesAttribute(array $value): void
     {
-        $this->attributes['types'] = implode(',', $value);
+        $this->attributes['job_types'] = implode(',', $value);
     }
 
-    public function getTypesAttribute(): array
+    public function getJobTypesAttribute(): array
     {
-        return explode(',', $this->attributes['types']);
+        return explode(',', $this->attributes['job_types']);
+    }
+
+    public function scopeForJob(Builder $query, Job $job): Builder
+    {
+        return $query->withCount([
+                    'keywords',
+                    'keywords as matching_keywords_count' => function (Builder $query) use ($job) {
+                        $query->whereRaw('instr(:description, word)', ['description' => $job->description]);
+                    }])
+                    ->havingRaw(
+                        'case when has_all_keywords = 1
+                            then keywords_count = matching_keywords_count
+                            else matching_keywords_count > 0
+                        end'
+                    );
     }
 }
