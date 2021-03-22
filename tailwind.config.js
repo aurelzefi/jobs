6afd629f8c8e6b275b7255e4af449374513daffd
Aurel Zefi <aurelzefi1994@gmail.com>
2021-03-22 22:15:19 +0100

    add purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+    purge: [
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+        './resources/js/**/**/*.vue'
+    ],
+
     theme: {
         extend: {
             fontFamily: {
